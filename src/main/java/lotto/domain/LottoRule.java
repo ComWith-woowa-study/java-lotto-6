5645eb895ce37b06adff02040fc8ae3785e46780
smini <sm020320@naver.com>
2025-10-01 17:38:10 +0900

    feat(domain): LottoRule 상수 정의 추가

---
+++ src/main/java/lotto/domain/LottoRule.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class LottoRule {
+    public static final int PRICE = 1000;
+    public static final int MIN = 1;
+    public static final int MAX = 45;
+    public static final int SIZE = 6;
+
+    private LottoRule() {}
+}
