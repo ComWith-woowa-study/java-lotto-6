16035eb8af3f9b1e8fb181bb86880efe36ed7e7a
smini <sm020320@naver.com>
2025-10-02 12:04:32 +0900

    style(view): OutputView 주석 제거

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -24,7 +24,6 @@ public class OutputView {
         System.out.println("5개 일치 (1,500,000원) - " + determineRank.get(Rank.THIRD) + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + determineRank.get(Rank.SECOND) + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + determineRank.get(Rank.FIRST) + "개");
-        // 여기 정리하기
         System.out.println("총 수익률은 " + String.format("%,.1f%%", LottoStatistic) + "입니다.");
     }
 
